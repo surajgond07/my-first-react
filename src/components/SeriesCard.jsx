926dd45724224dd822fc0ccd96d052862848405e
Suraj Gond <surajgond07@gmail.com>
2025-09-04 08:30:36 +0530

    Inline CSS Styling added in React

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,6 +2,12 @@ export const SeriesCard = ({curElem}) => {
   const { image_url, name, rating, description, genre, cast, watch_url } = curElem;
   console.log(curElem);
 
+  const btnStyle={
+          padding: "1.2rem 2.4rem",
+          border: "none",
+          fontSize: "1.6rem",
+        }
+
   return (
     <li className="card">
       <div>
@@ -15,7 +21,7 @@ export const SeriesCard = ({curElem}) => {
       <p><strong>Genre:</strong> {genre.join(", ")}</p>
       <p><strong>Cast:</strong> {cast.join(", ")}</p>
       <a href={watch_url} target="_blank" rel="noreferrer">
-        <button>Watch Now</button>
+        <button style={{btnStyle}}>Watch Now</button>
       </a>
       </div>
     </li>
