1b660777f51efcb51e21176e6cf65d53f0581ada
Suraj Gond <surajgond07@gmail.com>
2025-09-11 08:43:24 +0530

    Conditional Styling Apply on Rating

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,11 +2,12 @@ export const SeriesCard = ({curElem}) => {
   const { image_url, name, rating, description, genre, cast, watch_url } = curElem;
   console.log(curElem);
 
-  const btnStyle={
-          padding: "1.2rem 2.4rem",
-          border: "none",
-          fontSize: "1.6rem",
-        }
+  // const btnStyle={
+  //         padding: "1.2rem 2.4rem",
+  //         border: "none",
+  //         fontSize: "1.6rem",
+  //       }
+  const ratingClass = rating >= 8.5 ? "super-hit": "average";
 
   return (
     <li className="card">
@@ -16,12 +17,12 @@ export const SeriesCard = ({curElem}) => {
 
       <div className="card__content">
       <h2><strong>Name:</strong> {name}</h2>
-      <h3><strong>Rating:</strong> {rating}</h3>
+      <h3><strong>Rating: <span className={`rating ${ratingClass}`}> {rating}</span></strong></h3>
       <p><strong>Summary:</strong> {description}</p>
       <p><strong>Genre:</strong> {genre.join(", ")}</p>
       <p><strong>Cast:</strong> {cast.join(", ")}</p>
       <a href={watch_url} target="_blank" rel="noreferrer">
-        <button style={{btnStyle}}>Watch Now</button>
+        <button>Watch Now</button>
       </a>
       </div>
     </li>
