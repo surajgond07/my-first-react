b9200d25d3681dc4e64f4ab39f1ccde112143134
Suraj Gond <surajgond07@gmail.com>
2025-11-19 07:32:39 +0530

    Passing Events as Props in React

--- src/components/EventProps.jsx
+++ src/components/EventProps.jsx
@@ -1,8 +1,36 @@
 export const EventProps =() =>{
+    const HnadleWelcomeUser =(user) =>{
+        
+        alert(`Hey,${user}`);
+    }
+      const handleHover =() =>{
+        
+        alert(`Hey Thanks for Hove me`);
+    }
 return (
     <>
-    <WelcomeUser />
+    <WelcomeUser
+     onButtonClick ={() =>HnadleWelcomeUser('Suraj Gond')} onMouseEnter={handleHover}/>
     
     </>
-)
+);
+};
+
+const WelcomeUser = (props) =>{
+    const {onButtonClick, onMouseEnter} = props;
+    const handleGreeting =() =>{
+        console.log(`Welcome to our Website`);
+        onButtonClick();
+    }
+    return(
+        <>
+        <button onClick = {onButtonClick}>Click</button>
+        <button onMouseEnter = {onMouseEnter}>Hover Me</button>
+        <button onClick ={handleGreeting}>Greeting</button>
+        </>
+    )
 }
+
+
+
+
