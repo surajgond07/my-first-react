bc9849b4e3b206d47261569fcf0af49d502a898a
Suraj Gond <surajgond07@gmail.com>
2026-01-13 08:36:12 +0530

    Lifting State Up Completed

---
+++ src/components/LiftStateUp.jsx
@@ -0,0 +1,31 @@
+
+import {useState} from "react";
+export const LiftingState = () => {
+    const [inputValue, setInputValue] = (useState(""));
+    return <>
+    <InputComponent inputValue={inputValue} setInputValue={setInputValue} />
+    <DisplayComponent inputValue={inputValue} />
+    </>
+}
+
+
+const InputComponent = ({inputValue, setInputValue}) =>{
+    
+    return (
+    <>
+
+    <input
+     type="text" 
+    placeholder="Enter Your Name" 
+    value= {inputValue}
+        onChange = {(e) => setInputValue(e.target.value)} />
+    
+    
+  
+    </>
+    );
+}
+
+const DisplayComponent = ({inputValue}) =>{
+    return <p>The current input value is: {inputValue}</p>
+}
