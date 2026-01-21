b44816dc110b9070525274a7c8ccd9a72e968d0c
Suraj Gond <surajgond07@gmail.com>
2026-01-21 08:51:03 +0530

    Toggle Switch Completed

--- src/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import "./ToggleSwitch.css";
 export const ToggleSwitch = () =>{
     const [isOn, setIsOn] = useState(false);
+    const handleToggleSwitch= () => {
+        setIsOn(!isOn);
+    }
+
+    const checkIsOn = isOn ? "on" : "off";
+    
+    const toggleBGColor= {backgroundColor: isOn ? "#4caf50"  : ""  };
 
     return(
-        <div className="toggle-switch">
+        <div className="toggle-switch" style={toggleBGColor} onClick= {handleToggleSwitch}>
 
-            <div className="switch">
-                <span className="switch-state">{isOn ? "ON" : "OFF"}</span>
+            <div className={`switch ${checkIsOn} `}>
+                <span className="switch-state">{checkIsOn}</span>
             </div>
 
         </div>
